8948255afc13c9c014906f44130414e7f8e26f71
Andy Sisinger <andocmdo@gmail.com>
2018-01-07 23:37:13 -0500

    todo comments

--- main.go
+++ main.go
@@ -2,15 +2,18 @@ package main
 
 import (
 	"log"
-	"os/exec"
 	"runtime"
 	"strconv"
-	"time"
 )
 
-func init() {
+// TODO parameterize these
+const keepFreeCores = 1
+const startPort = 80085
+const masterIP = "192.168.1.32"
+const masterPort = "8080"
+
+func main() {
 	// number of processor cores to keep free, the rest will be used to run jobs
-	const keepFreeCores = 1
 	cores := 1
 	coresAvailable := runtime.NumCPU()
 	log.Println("Number of processor cores available: " + strconv.FormatInt(int64(coresAvailable), 10))
@@ -19,35 +22,36 @@ func init() {
 	}
 	log.Println("Number of processor cores to use: ", cores)
 
-	jobsToRun = make(chan int, 1000)
-
 	for i := 0; i < cores; i++ {
-		go worker(i, jobsToRun)
+		go worker(i)
 	}
 }
 
-func main() {
-	exit(1)
-}
-
-func worker(w int, jobChan <-chan int) {
+func worker(w int) {
 	log.Printf("started worker %d", w)
 
-	for id := range jobChan {
-		log.Printf("worker %d started job %d", w, id)
-		job, err := RepoFindJob(id)
-		if err != nil {
-			log.Printf("error on job %d", id)
-			log.Printf(err.Error())
-		}
-		job.Running = true
-		job.Started = time.Now()
-		_, err = RepoUpdateJob(job)
-		if err != nil {
-			log.Printf("error on job %d", id)
-			log.Printf(err.Error())
-		}
+	// register worker with gostockd api server
 
+	// open listening port for jobs
+
+	// while loop here
+	// update server showing this worker as ready to accept jobs
+
+	// wait/listen to port for incoming jobs
+
+	// decode incoming job
+
+	// start job
+
+	// update server that we have started job
+
+	// wait for job to finish
+
+	// update server of job completion status
+	// back to while loop
+
+	/*
+		id := 69
 		// This is where we would process our job
 		cmd := exec.Command("bash", "-c", "sleep 10; date")
 		out, err := cmd.Output()
@@ -58,17 +62,19 @@ func worker(w int, jobChan <-chan int) {
 		log.Printf("Job %d output: %s", id, out)
 
 		// And when finished, note the time, check for errors, etc
-		job.Ended = time.Now()
-		job.Running = false
-		job.Completed = true
-		job.Result = string(out)
 
-		_, err = RepoUpdateJob(job)
-		if err != nil {
-			log.Printf("error on job %d", id)
-			log.Printf(err.Error())
-			return
-		}
-		log.Printf("worker %d finished job %d", w, id)
-	}
+				job.Ended = time.Now()
+				job.Running = false
+				job.Completed = true
+				job.Result = string(out)
+
+
+			_, err = RepoUpdateJob(job)
+			if err != nil {
+				log.Printf("error on job %d", id)
+				log.Printf(err.Error())
+				return
+			}
+			log.Printf("worker %d finished job %d", w, id)
+	*/
 }
