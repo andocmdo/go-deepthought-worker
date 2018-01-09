b380ab2f6621da65131e3a986c959f426e0f2d2f
Andy Sisinger <andocmdo@gmail.com>
2018-01-09 15:27:38 -0500

    implement setReady and register methods for worker

--- worker.go
+++ worker.go
@@ -32,38 +32,17 @@ func (wrkr *worker) run(wn int) {
 		log.Printf(err.Error())
 	}
 	log.Printf("worker %d: Successfully registered with master server", wn)
-	master.Valid = true
-	master.LastContact = time.Now()
-	master.LastUpdate = time.Now()
+
 	// open listening port for jobs
 	// TODO 0mq stuff here
 
 	// loop here
 	for {
 		// update server showing this worker as ready to accept jobs
-		// set this worker as ready to accept jobs
-		wrkr.Ready = true
-		jsonWorker, _ := json.Marshal(wrkr)
-		resp, err := http.Post(master.URLworkers, jsonData, bytes.NewBuffer(jsonWorker))
-		//resp, err := http.PostForm(requestURL, url.Values{"port": {sPort}})
-		if err != nil {
-			log.Printf("worker %d: error setting READY with master server", wn)
-			log.Println(err)
-			return
-		}
-		body, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
+		if err := wrkr.setReady(); err != nil {
+			//handle error
+			log.Printf("worker %d: Error setting READY with master server", wn)
 			log.Printf(err.Error())
-			return
-		}
-		resp.Body.Close()
-		if err = json.Unmarshal(body, &wrkr); err != nil {
-			log.Printf(err.Error())
-			return
-		}
-		if wrkr.Valid != true {
-			log.Printf("worker %d: master server returned worker object with false VALID flag when setting READY!", wn)
-			return
 		}
 		log.Printf("worker %d: Successfully notified master server, READY to accept jobs", wn)
 
@@ -97,17 +76,17 @@ func (wrkr *worker) register() error {
 	//resp, err := http.PostForm(requestURL, url.Values{"port": {wrkr.Port}})
 	if err != nil {
 		//log.Printf("worker %d: error registering with master server", wn)
-		log.Println(err)
+		log.Print("error registering worker with master server: ", err.Error())
 		return err
 	}
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		log.Printf(err.Error())
+		log.Print("error registering worker with master server: ", err.Error())
 		return err
 	}
 	resp.Body.Close()
 	if err = json.Unmarshal(body, &wrkr); err != nil {
-		log.Printf(err.Error())
+		log.Print("error registering worker with master server: ", err.Error())
 		return err
 	}
 	if wrkr.Valid != true {
@@ -115,5 +94,41 @@ func (wrkr *worker) register() error {
 		return errors.New("master server response was returned as invalid")
 	}
 	//log.Printf("worker %d: Successfully registered with master server", wn)
+	master.Valid = true
+	master.LastContact = time.Now()
+	master.LastUpdate = time.Now()
+
+	return nil
+}
+
+func (wrkr *worker) setReady() error {
+	// set this worker as ready to accept jobs
+	wrkr.Ready = true
+	jsonWorker, _ := json.Marshal(wrkr)
+	resp, err := http.Post(master.URLworkers, jsonData, bytes.NewBuffer(jsonWorker))
+	//resp, err := http.PostForm(requestURL, url.Values{"port": {sPort}})
+	if err != nil {
+		//log.Printf("worker %d: error setting READY with master server", wn)
+		//log.Println(err)
+		return err
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		//log.Printf(err.Error())
+		return err
+	}
+	resp.Body.Close()
+	if err = json.Unmarshal(body, &wrkr); err != nil {
+		//log.Printf(err.Error())
+		return err
+	}
+	if wrkr.Valid != true {
+		//log.Printf("worker %d: master server returned worker object with false VALID flag when setting READY!", wn)
+		return errors.New("master server response was returned as invalid")
+	}
+	master.Valid = true
+	master.LastContact = time.Now()
+	master.LastUpdate = time.Now()
+
 	return nil
 }
