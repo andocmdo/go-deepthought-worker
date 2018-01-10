b619d07aeac7837e5b3ea20057f6231b1fd38395
Andy Sisinger <andocmdo@gmail.com>
2018-01-09 22:14:56 -0500

    bugfix ready, working, register, and job running

--- worker.go
+++ worker.go
@@ -7,6 +7,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
 	"time"
 )
 
@@ -24,15 +25,15 @@ type Worker struct {
 }
 
 func (wrkr *Worker) run(wn int, master Server) {
-	log.Printf("worker thread %d: started", wn)
+	log.Printf("thread %d: started", wn)
 
 	// register worker with gostockd api server
 	if err := wrkr.register(&master); err != nil {
 		// handle error
-		log.Printf("worker %d: Error registering with master server", wn)
+		log.Printf("thread %d: Error registering with master server", wn)
 		log.Printf(err.Error())
 	}
-	log.Printf("worker %d: Successfully registered with master server", wn)
+	log.Printf("thread %d: Successfully registered with master server as ID %d", wn, wrkr.ID)
 
 	// open listening port for jobs
 	job := NewJob() // initialize an empty job to place incoming JSON job
@@ -43,37 +44,43 @@ func (wrkr *Worker) run(wn int, master Server) {
 		// update server showing this worker as ready to accept jobs
 		if err := wrkr.setReady(&master); err != nil {
 			//handle error
-			log.Printf("worker %d: Error setting READY with master server", wn)
+			log.Printf("thread %d: Error setting READY with master server as ID %d", wn, wrkr.ID)
 			log.Printf(err.Error())
 		}
-		log.Printf("worker %d: Successfully notified master server, READY to accept jobs", wn)
+		log.Printf("thread %d: Successfully notified master server, READY to accept jobs as ID %d", wn, wrkr.ID)
 
 		// wait/listen to port for incoming jobs
 		time.Sleep(time.Second * 15)
 
 		// decode incoming job
-		log.Printf("worker %d: recieved job", wn)
+		log.Printf("thread %d: recieved job as ID %d", wn, wrkr.ID)
 		time.Sleep(time.Second * 5)
 
 		// start job
-		log.Printf("worker %d: started job", wn)
+		log.Printf("thread %d: started job as ID %d", wn, wrkr.ID)
 		time.Sleep(time.Second * 5)
 
 		// update server that we are working, and that job is running on this worker
 		if err := wrkr.setWorking(&master, job); err != nil {
 			//handle error
-			log.Printf("worker %d: Error setting READY with master server", wn)
+			log.Printf("thread %d: Error setting WORKING with master server as ID %d", wn, wrkr.ID)
 			log.Printf(err.Error())
 		}
-		log.Printf("worker %d: updated master for running job NUMBER WHAT?", wn)
-		time.Sleep(time.Second * 5)
+		log.Printf("thread %d: updated master for running job NUMBER WHAT? as ID %d", wn, wrkr.ID)
+		if err := job.setRunning(&master, wrkr); err != nil {
+			//handle error
+			log.Printf("thread %d: Error setting job running with master server as ID %d", wn, wrkr.ID)
+			log.Printf(err.Error())
+		}
+		log.Printf("thread %d: updated master for running job NUMBER WHAT? as ID %d", wn, wrkr.ID)
+		time.Sleep(time.Second * 25)
 
 		// wait for job to finish
-		log.Printf("worker %d: completed job", wn)
-		time.Sleep(time.Second * 5)
+		log.Printf("thread %d: completed job ?? as worker ID %d", wn, wrkr.ID)
 
 		// update server of job completion status
-		log.Printf("worker %d: updated master of successful job completion", wn)
+
+		log.Printf("thread %d: updated master of successful job ?? completion as worker ID %d", wn, wrkr.ID)
 	}
 }
 
@@ -112,7 +119,7 @@ func (wrkr *Worker) setReady(master *Server) error {
 	// set this worker as ready to accept jobs
 	wrkr.Ready = true
 	jsonWorker, _ := json.Marshal(wrkr)
-	resp, err := http.Post(master.URLworkers, jsonData, bytes.NewBuffer(jsonWorker))
+	resp, err := http.Post(master.URLworkers+"/"+strconv.Itoa(wrkr.ID), jsonData, bytes.NewBuffer(jsonWorker))
 	//resp, err := http.PostForm(requestURL, url.Values{"port": {sPort}})
 	if err != nil {
 		//log.Printf("worker %d: error setting READY with master server", wn)
@@ -146,7 +153,7 @@ func (wrkr *Worker) setWorking(master *Server, job *Job) error {
 	wrkr.Working = true
 	wrkr.JobID = job.ID
 	jsonWorker, _ := json.Marshal(wrkr)
-	resp, err := http.Post(master.URLworkers, jsonData, bytes.NewBuffer(jsonWorker))
+	resp, err := http.Post(master.URLworkers+"/"+strconv.Itoa(wrkr.ID), jsonData, bytes.NewBuffer(jsonWorker))
 	//resp, err := http.PostForm(requestURL, url.Values{"port": {sPort}})
 	if err != nil {
 		//log.Printf("worker %d: error setting READY with master server", wn)
@@ -175,5 +182,33 @@ func (wrkr *Worker) setWorking(master *Server, job *Job) error {
 }
 
 func (job *Job) setRunning(master *Server, wrkr *Worker) error {
+	job.Running = true
+	job.WorkerID = wrkr.ID
+	jsonWorker, _ := json.Marshal(*job)
+	resp, err := http.Post(master.URLjobs+"/"+strconv.Itoa(job.ID), jsonData, bytes.NewBuffer(jsonWorker))
+	//resp, err := http.PostForm(requestURL, url.Values{"port": {sPort}})
+	if err != nil {
+		//log.Printf("worker %d: error setting READY with master server", wn)
+		//log.Println(err)
+		return err
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		//log.Printf(err.Error())
+		return err
+	}
+	resp.Body.Close()
+	if err = json.Unmarshal(body, &wrkr); err != nil {
+		//log.Printf(err.Error())
+		return err
+	}
+	if wrkr.Valid != true {
+		//log.Printf("worker %d: master server returned worker object with false VALID flag when setting READY!", wn)
+		return errors.New("master server response was returned as invalid")
+	}
+	master.Valid = true
+	master.LastContact = time.Now()
+	master.LastUpdate = time.Now()
+
 	return nil
 }
